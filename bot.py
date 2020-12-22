0e234ab3b4320e7d44c780a10d7dfc55f32e5585
Violet <Violet.Eqz@gmail.com>
2020-12-23 01:24:13 +0700

    ok

--- bot.py
+++ bot.py
@@ -11,8 +11,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 
-
-@bot.command(name='Fact', help='Những kiến thức bổ ích của thầy Huấn')
+@bot.command(name='fact', help='Những kiến thức bổ ích của thầy Huấn')
 async def nine_nine(ctx):
     brooklyn_99_quotes = [
         'Manh Hung la mot con cho.',
@@ -21,5 +20,30 @@ async def nine_nine(ctx):
 
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
+
+@bot.command(name='dm', help='Những kiến thức bổ ích của thầy Huấn')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'Manh Hung sua gau gau.'
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+@bot.command(name='handsome', help='Thu do dep trai cua ban.')
+async def _bot(ctx):
+    """Bot co dep trai khong?"""
+    await ctx.send('Lai chang dep trai vl.')
+
+@bot.command(name='add')
+async def add(ctx, x1: int, x2: int):
+    """Adds two numbers together."""
+    await ctx.send(x1 + x2)
+
+@bot.command(name='spam')
+async def repeat(ctx, times: int):
+    content = 'Nhat dep trai qua <3'
+    for i in range(times):
+        await ctx.send(content)
 
 bot.run(TOKEN)
