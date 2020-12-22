c8483102c92e9fa28d70d73b5221466654cf9cab
Violet <Violet.Eqz@gmail.com>
2020-12-22 21:50:07 +0700

    Tuyet voi toi da cuoi

--- bot.py
+++ bot.py
@@ -1,34 +1,25 @@
 # bot.py
 import os
 import random
-import discord
+
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
+@bot.command(name='Fact', help='Những kiến thức bổ ích của thầy Huấn')
+async def nine_nine(ctx):
     brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
+        'Manh Hung la mot con cho.',
+        'Nhat dep trai vl.'
     ]
 
-    if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
 
-client.run(TOKEN)
+bot.run(TOKEN)
